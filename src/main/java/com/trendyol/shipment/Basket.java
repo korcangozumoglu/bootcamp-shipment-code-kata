4860a9d9587445332a93c49a460078c39128fe4a
Korcan Gözümoğlu <34817700+korcangozumoglu@users.noreply.github.com>
2023-10-02 21:48:27 +0300

    Implement getShipmentSize in Basket Class and Refactor

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,45 @@
 package com.trendyol.shipment;
 
-import java.util.List;
+import java.util.*;
 
 public class Basket {
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        if (products.size() < 3)
+            return getBiggestSize();
+        Map<ShipmentSize, Integer> productsSizeMap = getRepeatMap();
+        ShipmentSize mostRepeatedSize = findShipmentSizeWithValueAtLeastThree(productsSizeMap);
+        return mostRepeatedSize != null ? mostRepeatedSize.getNext() : getBiggestSize();
+    }
+
+    private Map<ShipmentSize, Integer> getRepeatMap() {
+        Map<ShipmentSize, Integer> productsSizeMap = new EnumMap<>(ShipmentSize.class);
+        for (Product product : products) {
+            if (!productsSizeMap.containsKey(product.getSize())) {
+                productsSizeMap.put(product.getSize(), 1);
+            } else {
+                productsSizeMap.put(product.getSize(), productsSizeMap.get(product.getSize()) + 1);
+            }
+        }
+        return productsSizeMap;
+    }
+
+    private ShipmentSize findShipmentSizeWithValueAtLeastThree(Map<ShipmentSize, Integer> productsSizeMap) {
+
+        return productsSizeMap.entrySet().stream()
+                .filter(product -> product.getValue() > 2)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private ShipmentSize getBiggestSize() {
+        return products.stream()
+                .map(Product::getSize)
+                .max(Comparator.naturalOrder())
+                .orElse(null);
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,17 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    public ShipmentSize getNext() {
+        if (this == getBiggest())
+            return getBiggest();
+        return values()[(ordinal() + 1) % values().length];
+    }
+
+    public ShipmentSize getBiggest() {
+        return values()[values().length - 1];
+    }
 }
+
+
